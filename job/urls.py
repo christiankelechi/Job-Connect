d46d4079914f9a2343a4f639377258316b1c853f
Ddilibe <franklinfidelugwuowo@gmail.com>
2024-11-18 04:56:41 +0100

    [Chore]: Completed the desktop view of the frontend

--- job/urls.py
+++ job/urls.py
@@ -7,8 +7,14 @@ urlpatterns = [
     path("about-us", job.about_us, name="aboutus"),
     path('contact_us', job.contact_us, name="contactus"),
     path("admin_login", job.admin_login, name="adminlogin"),
+    path('job_list', job.job_list_post, name="joblistpost"),
     path("admin_signup", job.admin_signup, name="adminsignup"),
     path('student_login', job.student_login, name="studentlogin"),
     path('student_signup', job.student_signup, name="studentsignup"),
-    path('job_list', job.job_list_post, name="joblistpost"),
+    path("single-job/<str:primary_key>", job.job_single, name="singlejob"),
+    path("reset_password", job.reset_password, name="resetpassword"),
+    path("change_password", job.change_password, name="changepassword"),
+    path("admin_profile", job.admin_profile, name="adminprofile"),
+    path("student_profile", job.student_profile, name="studentprofile"),
+    path("company_profile", job.company_profile, name="companyprofile")
 ]
--- job/views.py
+++ job/views.py
@@ -1,30 +1,48 @@
-from django.shortcuts import render
-from django.conf import settings
 from django.core.paginator import Paginator
+from django.shortcuts import render, redirect
 import datetime
 import uuid
 
 # Create your views here.
 jobs = [ 
         dict(
-            pk=uuid.uuid4(),
-            distance=30,
+            pk=str(uuid.uuid4()),
+            opening=30,
             title="software Engineer", 
             details="Experienced in Node.Js, Java, Spring, Bootstrap, Python and Selenium integration and Unit Testing",
             salary="26K - 30k Monthly",
             place='Poland',
             time=datetime.date.fromisoformat("2024-04-24").isoformat(),
             image_url="jobs/job_1.jpeg",
+            app_date=datetime.datetime.now(),
+            last_date=datetime.datetime.now(),
+            company_name="Microsoft"
         ),
         dict(
-            pk=uuid.uuid4(),
+            pk=str(uuid.uuid4()),
             title="mobile App Engineer",
             details="Experienced in Flutter or React native for uilding a startup app",
-            distance=10,
+            opening=10,
             salary="8k - 10K Monthly",
             place="USA",
             time=datetime.date.fromisoformat("2024-04-24").isoformat(),
-            image_url="jobs/job_2.jpeg"
+            image_url="jobs/job_2.jpeg",
+            app_date=datetime.datetime.now(),
+            last_date=datetime.datetime.now(),
+            company_name="Microsoft"
+        ),
+        dict(
+            pk=str(uuid.uuid4()),
+            title="mobile App Developer needed in Binance",
+            details="Binance is looking for senior mobile app developer with at least 10 years of experience building cross platform application with flutter, firebase and react native. The role is also remote based and also  willing to attend yearly meeting at binance head quarters in your country",
+            opening=10,
+            salary="50k - 100K Monthly",
+            place="USA",
+            time=datetime.date.fromisoformat("2024-04-24").isoformat(),
+            image_url="jobs/job_5.jpeg",
+            app_date=datetime.datetime.now(),
+            last_date=datetime.datetime.now(),
+            company_name="Microsoft"
         ),
     ]
 
@@ -58,8 +76,27 @@ def job_list_post(requests):
     context = {'jobs': jobs[(int(page_number)*length)-length: (int(page_number)*length)], 'page_obj': page_obj}
     if query := requests.GET.get("query"):
         context['query'] = query
+    print(jobs, end="\n\n")
     return render(requests, "job/job_list.html", context)
 
 def job_single(requests, primary_key):
-    single = [i for i in jobs if i['px'] == primary_key][0]
+    print(jobs, end="\n\n")
+    if not (single := [i for i in jobs if i['pk'] == primary_key]):
+        return redirect('landingpage')
+    single = single[0]
     return render(requests, "job/single_job.html", context={'single':single})
+
+def reset_password(requests):
+    return render(requests, "auth/reset_password.html")
+
+def change_password(requests):
+    return render(requests, "auth/change_password.html")
+
+def company_profile(requests):
+    return render(requests, "job/company_profile.html")
+
+def student_profile(requests):
+    return render(requests, "job/student_profile.html")
+
+def admin_profile(requests):
+    return render(requests, "job/admin_profile.html")
